b50a6cfad0fc1a62f38a4cb70363941cc63c5e8a
Giles Sandoval <gsandoval2277@gmail.com>
2019-08-02 23:25:11 -0600

    Create age_program_loc1713.py

---
+++ age_program_loc1713.py
@@ -0,0 +1,14 @@
+# program: age is assigned to integer that prints different strings based on age.
+# tested each if statement and elif statement before moving to next if statement
+# else statement didn't need formula
+
+age = 22
+
+if age <= 5:
+    print ("it's a bebe!")
+elif 5 < age <= 12:
+    print ("it's a kid")
+elif 12 < age <= 19:
+    print ("it's a teenager")
+else:
+    print ("it's an adult or older")
