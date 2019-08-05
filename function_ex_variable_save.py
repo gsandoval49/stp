ee6d0261ec763ee01ec42645759f84a27d97c8a0
Giles Sandoval <gsandoval2277@gmail.com>
2019-08-04 22:49:17 -0600

    Create function_ex_variable_save.py

---
+++ function_ex_variable_save.py
@@ -0,0 +1,13 @@
+# You can save the result your function returns in a variable 
+# whenever you need to use the value later in your program:
+
+def f(x):
+    return x + 1
+
+z = f(4)
+
+if z == 5:
+    print ("z is 5")
+else:
+    print ("z is not 5")
+
