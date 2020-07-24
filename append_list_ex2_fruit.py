61ecd65ec73c29772ae13ef849d4da998460c905
Giles Sandoval <gsandoval2277@gmail.com>
2020-07-24 02:12:11 -0600

    Create append_list_ex2_fruit.py

---
+++ append_list_ex2_fruit.py
@@ -0,0 +1,9 @@
+fruit = ["apple", "banana", "peach"]
+fruit.append("pear")
+fruit.append("orange")
+fruit
+
+# note: no space between append and (), didn't populate appends
+# when i ran the program. 
+
+# >>> ['apple', 'banana', 'peach', 'pear', 'orange']
