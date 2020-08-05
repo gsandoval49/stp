b56e778101d29732adc4629e101cccf290dcb9e9
Giles Sandoval <gsandoval2277@gmail.com>
2020-08-04 22:16:37 -0600

    Create list_input_if_else_ex137.py

---
+++ list_input_if_else_ex137.py
@@ -0,0 +1,10 @@
+# an ex. of how you might use a list in practice
+
+colors = ["green", "red", "blue"]
+
+guess = input("Please guess a color:")
+
+if guess in colors:
+    print ("You guessed correctly!")
+else:
+    print ("Negative! Try again please.")
