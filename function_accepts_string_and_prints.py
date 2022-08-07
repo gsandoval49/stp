437936f636eb516599d511361042c5d51d254fde
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-06 20:32:00 -0600

    initial commit

---
+++ function_accepts_string_and_prints.py
@@ -0,0 +1,16 @@
+# Challenge Ch4: #2 create a fucntion that accepts a string as a parameter and prints it
+
+a = input("enter a string:")
+
+if str:
+    print(a)
+else:
+    print ("this is not a string.")
+    
+
+# alternative
+
+def print_string(string):
+    print(string)
+
+print_string("Testring: 1,2,3.")
