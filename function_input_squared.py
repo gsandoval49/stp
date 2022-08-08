8208bfac94ab906b31d32d29fe306c6c1c3ab93f
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-08 10:39:43 -0600

    intiial commit with doc blocks

---
+++ function_input_squared.py
@@ -0,0 +1,9 @@
+a = input("type a number:")
+"""
+input is entered as a string
+need to convert to integer
+int can now be used as formula which should square the number entered.
+"""
+
+b = int(a)**2
+print("squared number is", b)
