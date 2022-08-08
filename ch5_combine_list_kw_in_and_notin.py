b94039a24770025154edf1f6ecca071cae6b8974
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-08 15:42:25 -0600

    added len function

--- ch5_combine_list_kw_in_and_notin.py
+++ ch5_combine_list_kw_in_and_notin.py
@@ -12,3 +12,7 @@ print("green" in colors3)
 #using the keyword not to check if an item is not in a list.
 
 print("periwinkle" not in colors3)
+
+
+# you can get the size (the number of items in it) of a list with the len function
+print(len(colors3))
