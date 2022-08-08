c0c0c2f861aa17a99f7221d6c340c087f64e11f9
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-08 15:33:21 -0600

    combine lists, kw: in and kw: not in

---
+++ ch5_combine_list_kw_in_and_notin.py
@@ -0,0 +1,14 @@
+colors1 = ["blue", "green", "yellow"]
+colors2 = ["red", "pink", "black"]
+
+# combine both lists
+colors3 = colors1 + colors2
+print(colors3)
+
+
+# you can check if an item is in a list with the keyword in:
+print("green" in colors3)
+
+#using the keyword not to check if an item is not in a list.
+
+print("periwinkle" not in colors3)
