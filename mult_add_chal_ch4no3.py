5e567f2c5c03c23ba84923c1cb7a6a8d4f0421ad
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-08 10:49:54 -0600

    intiial comit with doc strings
    >>>2500020

---
+++ mult_add_chal_ch4no3.py
@@ -0,0 +1,12 @@
+def mult_param(a,b,c,x=100,z=1000):
+    return a + b + c * x * z
+
+    """
+    returns and the multiplication of x * z
+    multiplied by the sum a + b + c
+    :param a,b,c: int and required
+    :param x,z: int and optional paramaters
+    """
+
+result = mult_param(10, 10, 25)
+print(result)
