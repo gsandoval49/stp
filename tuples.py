a564d8cdc27529ef35a19cd45117412afd226246
Giles Sandoval <20120630+gsandoval49@users.noreply.github.com>
2022-08-09 22:15:25 -0600

    initial commit

---
+++ tuples.py
@@ -0,0 +1,18 @@
+#syntax of a tuple
+my_tuple = tuple()
+my_tuple
+
+
+#example of a tupple
+rndm = ("M. Jackson", 1958, True)
+print(rndm)
+
+#tuple with a single item
+# it needs a comma after it to distinguish as a tuple.
+
+#if i try to edit the tupple we'll get a TypeError
+# rndm[1] = 1968
+
+####
+# >>> rndm[1] = 1968
+# >>> TypeError: 'tuple' object does not support item assignment
